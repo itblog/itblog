111f5bf03241b6d397b1ac588d12ad6e30e3d89f
yaozulv <yaozulv@gmail.com>
2024-04-03 20:29:40 +0800

    add shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>itblog</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center gap-4">
+      <h1 className="text-3xl">itblog</h1>
+      <Button>Click Me</Button>
     </main>
   )
 }
