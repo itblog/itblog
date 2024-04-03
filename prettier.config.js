32a8790dcf1ecb8d7854180ca889f990a8ed1ae5
yaozulv <yaozulv@gmail.com>
2024-04-03 20:07:50 +0800

    add prettier config

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  endOfLine: "lf",
+  plugins: ["prettier-plugin-tailwindcss"],
+}
