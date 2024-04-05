897e320144cf836a0333c9b8a7b37f8f564e96f2
yaozulv <yaozulv@gmail.com>
2024-04-05 16:22:31 +0800

    login ui

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -3,7 +3,7 @@ import { LoginForm } from "@/components/auth/login-form"
 export default async function Page() {
   return (
     <main>
-      <div className="flex flex-col items-center py-20">
+      <div className="flex min-h-[calc(100vh-150px)] flex-col items-center justify-center">
         <LoginForm />
       </div>
     </main>
