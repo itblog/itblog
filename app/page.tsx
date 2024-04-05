b635ecc3985a09092c877576f24d238a13868f6c
yaozulv <yaozulv@gmail.com>
2024-04-05 18:47:31 +0800

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default async function Page() {
   return (
     <main className="mx-auto my-6 max-w-4xl space-y-6">
-      <h1 className="break-words text-3xl">Home</h1>
+      <div className="text-center">
+        <h1 className="break-words text-3xl">Home</h1>
+      </div>
     </main>
   )
 }
