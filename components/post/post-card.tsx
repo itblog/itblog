2e1599470f18fd8d694aa84b03c3577c607627df
yaozulv <yaozulv@gmail.com>
2024-04-07 17:01:44 +0800

    Initial commit

--- components/post/post-card.tsx
+++ components/post/post-card.tsx
@@ -1,26 +1,26 @@
-import { type Post } from "@/models/post"
+import { type PostWithUser } from "@/models/post"
 import { type WithId } from "mongodb"
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "../ui/card"
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar"
 
-export const PostCard = ({ post }: { post: WithId<Post> }) => {
+export const PostCard = ({ post }: { post: WithId<PostWithUser> }) => {
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>
-          <time dateTime={post.createdAt?.toLocaleString()}>
-            {post.createdAt?.toLocaleString()}
-          </time>
-        </CardTitle>
-      </CardHeader>
-      <CardContent>
-        <p className="whitespace-pre-wrap">{post.content}</p>
-      </CardContent>
-    </Card>
+    <div className="flex gap-4">
+      <Avatar>
+        <AvatarImage src={post.user?.image || ""} alt={post.user?.name || ""} />
+        <AvatarFallback>{post.user?.name?.slice(0, 1)}</AvatarFallback>
+      </Avatar>
+      <div className="space-y-1">
+        <div>
+          <h1 className="font-semibold">{post.user?.name}</h1>
+        </div>
+        <div className="whitespace-pre-wrap">{post.content}</div>
+        <time
+          className=" text-xs text-muted-foreground"
+          dateTime={post.createdAt?.toLocaleString()}
+        >
+          {post.createdAt?.toLocaleString()}
+        </time>
+      </div>
+    </div>
   )
 }
--- models/post.ts
+++ models/post.ts
@@ -1,11 +1,16 @@
-import { ObjectId } from "mongodb"
+import { ObjectId, WithId } from "mongodb"
 import { db } from "./db"
+import { User } from "next-auth"
 
 export interface Post {
   userId: ObjectId
   content: string
   createdAt?: Date
   updatedAt?: Date
+}
+
+export interface PostWithUser extends Post {
+  user: WithId<User>
 }
 
 export const createPost = async (data: Post) => {
@@ -21,8 +26,35 @@ export const createPost = async (data: Post) => {
   return insertedId
 }
 
+export const getPosts = async () => {
+  const posts = db.collection<Post>("posts")
+  const docs = await posts.find().limit(10).toArray()
+  return docs
+}
+
 export const getLatestPosts = async () => {
   const posts = db.collection<Post>("posts")
-  const docs = await posts.find().sort({ _id: -1 }).limit(50).toArray()
+  const pipeline = []
+  pipeline.push({
+    $lookup: {
+      from: "users",
+      localField: "userId",
+      foreignField: "_id",
+      as: "_user",
+    },
+  })
+  pipeline.push({
+    $set: {
+      user: {
+        $first: "$_user",
+      },
+    },
+  })
+  const docs = (await posts
+    .aggregate(pipeline)
+    .limit(3)
+    .sort({ _id: -1 })
+    .toArray()) as WithId<PostWithUser>[]
+
   return docs
 }
